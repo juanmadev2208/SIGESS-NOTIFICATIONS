b48b058a6e10eec82c86be08aeb934e0dc3e2131
juanma0822 <yrd2dsh9rg@privaterelay.appleid.com>
2026-03-25 18:41:42 -0500

    fix: bind server to 0.0.0.0 for external access

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const { env } = require("./config/env");
 
-app.listen(env.PORT, () => {
+app.listen(env.PORT, "0.0.0.0", () => {
   console.log(`[notifications-ms] running on port ${env.PORT} (${env.NODE_ENV})`);
 });
